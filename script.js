810addec8de3b5e8ec1c03529f93edf62d75c0ee
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-03-11 21:30:06 +0530

    Add transaction function

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ const localStorageTransaction = JSON.parse(localStorage.getItem('transactions'))
 
 let transactions =
   localStorage.getItem('transactions') !== null ? localStorageTransaction : []
+
+addTransaction = (e) => {
+  e.preventDefault()
+
+  if (text.value.trim() === '') {
+    alert('Please add text')
+  } else if (amount.value.trim === '') {
+    alert('Please add amount')
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: amount.value,
+    }
+    transactions.push(transaction)
+    addTransactionDOM(transaction)
+    updateValues()
+    updateLocalStorage()
+
+    text.value = ''
+    amount.value = ''
+  }
+}
+
+init = () => {
+  list.innerHTML = ''
+  transactions.forEach(addTransactionDOM)
+  updateValues()
+}
+init()
+form.addEventListener('submit', addTransaction)
